7f1a60f8c73fd4fe7f80862dc6942fa3a3894e2d
hanyu <nozidoali@gmail.com>
2025-05-21 18:58:19 -0700

    update: clean the data folder

--- data/tmp.py
+++
@@ -1,9 +0,0 @@
-import os
-
-for filename in os.listdir('.'):
-    if filename.endswith('_kernel.c'):
-        # rename it to X.c
-        os.rename(filename, filename.replace('_kernel.c', '.c'))
-    elif filename.endswith('_ds_config.json'):
-        # rename it to X.json
-        os.rename(filename, filename.replace('_ds_config.json', '.json'))
